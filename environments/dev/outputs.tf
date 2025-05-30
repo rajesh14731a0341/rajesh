d152753683dd87e32b797167d08587e6abdb758f
rajesh14731a0341 <81812171+rajesh14731a0341@users.noreply.github.com>
2025-05-30 15:33:15 +0530

    Delete environments directory

--- environments/dev/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "ecs_cluster_id" {
-  value = module.ecs.ecs_cluster_id
-}
-
-output "ecs_service_name" {
-  value = module.ecs.ecs_service_name
-}
--- environments/dev/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-container_image = "YOUR_AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/grafana:latest"
-admin_password  = "admin"
